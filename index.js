b297e9799744f751840e08c193aa0e00cd1a8a64
Nicolas Franchini <devnicolasfranchini@gmail.com>
2022-04-09 23:24:03 -0300

    Modificaciones generales

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 // Bienvenida
 
-let nombre = prompt("Ingrese su nombre");
+let nombreUsuario = prompt("Ingrese su nombre");
 let apellido = prompt("Ingrese su apellido");
 let celular = parseInt(prompt("Ingrese su número de celular"));
 let contra = prompt("Ingrese su constraseña");
 
 alert(
-  `¡Hola ${nombre}! Le damos la bienvenida a Las Flores Pastelería. Vamos a verificar sus datos para poder realizar una compra segura...`
+  `¡Hola ${nombreUsuario}! Le damos la bienvenida a Las Flores Pastelería. Vamos a verificar sus datos para poder realizar una compra segura...`
 );
 
 let user = "";
 let pass = "";
 let intentos = 3;
 
-function login(nombre, contrasenia, intentos) {
-  if (!(nombre === "" && contrasenia === "")) {
-    while (intentos > 0 && (nombre !== user || contrasenia !== pass)) {
+function login(nombreUsuario, contrasenia, intentos) {
+  if (!(nombreUsuario === "" && contrasenia === "")) {
+    while (intentos > 0 && (nombreUsuario !== user || contrasenia !== pass)) {
       user = prompt("Ingrese su nombre");
       pass = prompt("Ingrese su contraseña");
 
-      if (user === nombre && pass === contrasenia) {
+      if (user === nombreUsuario && pass === contrasenia) {
         alert(`¡Datos verificados ${user}! Disfrute su compra.`);
       } else {
         alert(
@@ -30,14 +30,17 @@ function login(nombre, contrasenia, intentos) {
   }
 }
 
-login(nombre, contra, intentos);
+login(nombreUsuario, contra, intentos);
+
+console.log(`El usuario que ha ingresado es ${nombreUsuario} ${apellido}`);
 
 // Creo objetos
 
 class Productos {
-  constructor(nombre, precio) {
+  constructor(nombre, precio, cantidad) {
     this.nombre = nombre.toUpperCase();
     this.precio = parseFloat(precio);
+    this.cantidad = parseFloat(cantidad);
     this.vendido = false;
   }
 
@@ -46,12 +49,12 @@ class Productos {
   }
 }
 
-const torta = new Productos("Torta", 2500);
-const budin = new Productos("Budin", 1000);
-const pan = new Productos("Pan", 200);
-const tarta = new Productos("Tarta", 500);
-const galleta = new Productos("Galleta", 100);
-const helado = new Productos("Helado", 50);
+const torta = new Productos("Torta", 2500, 4);
+const budin = new Productos("Budin", 1000, 5);
+const pan = new Productos("Pan", 200, 10);
+const tarta = new Productos("Tarta", 500, 1);
+const galleta = new Productos("Galleta", 100, 20);
+const helado = new Productos("Helado", 50, 2);
 
 // Creo arrays
 
@@ -61,22 +64,28 @@ const productosInactivos = [];
 function elegirProductos() {
   let productoSeleccionado = parseInt(
     prompt(
-      `${nombre}, ingrese 1 para torta\nIngrese 2 para budin\nIngrese 3 para pan\nIngrese 4 para tarta\nIngrese 5 para galleta\nIngrese 6 para helado\nPuede elegir hasta 3 productos.`
+      `${nombreUsuario}, ingrese 1 para torta\nIngrese 2 para budin\nIngrese 3 para pan\nIngrese 4 para tarta\nIngrese 5 para galleta\nIngrese 6 para helado\nPuede elegir hasta 3 productos.`
     )
   );
 
   if (productoSeleccionado === 1) {
     var nombre = torta;
+    console.log(`Restan ${torta.cantidad - 1} tortas.`);
   } else if (productoSeleccionado === 2) {
     var nombre = budin;
+    console.log(`Restan ${budin.cantidad - 1} budines.`);
   } else if (productoSeleccionado === 3) {
     var nombre = pan;
+    console.log(`Restan ${pan.cantidad - 1} panes.`);
   } else if (productoSeleccionado === 4) {
     var nombre = tarta;
+    console.log(`Restan ${tarta.cantidad - 1} tartas.`);
   } else if (productoSeleccionado === 5) {
     var nombre = galleta;
+    console.log(`Restan ${galleta.cantidad - 1} galletas.`);
   } else if (productoSeleccionado === 6) {
     var nombre = helado;
+    console.log(`Restan ${helado.cantidad - 1} helados.`);
   } else alert("Producto inexistente o ya seleccionado");
 
   function cambiar(nombre) {
